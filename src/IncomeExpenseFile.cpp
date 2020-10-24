41d2393e7e36709c71407fa590fd853c8cd5818f
Maciej Anioł <maciej.aniol.programista@gmail.com>
2020-10-24 17:49:07 +0200

    Bug with loading wrong etries from file fixed

--- src/IncomeExpenseFile.cpp
+++ src/IncomeExpenseFile.cpp
@@ -13,13 +13,13 @@ void IncomeExpenseFile::getLastEntryIdFromFile()
     xml.Load( FILE_NAME );
     while ( xml.FindElem("ENTRY"))
     {
-    xml.IntoElem();
+        xml.IntoElem();
 
-    xml.FindElem( "ID" );
-    currentId = atoi( MCD_2PCSZ(xml.GetData()));
-    if ( currentId  > lastId )
-        lastId = currentId;
-    xml.OutOfElem();
+        xml.FindElem( "ID" );
+        currentId = atoi( MCD_2PCSZ(xml.GetData()));
+        if ( currentId  > lastId )
+            lastId = currentId;
+        xml.OutOfElem();
     }
     lastEntryId=lastId;
     return;
@@ -46,33 +46,41 @@ void IncomeExpenseFile::appendEntryToFile(Entry entry)
 vector <Entry> IncomeExpenseFile::loadEntriesFromFile(int loggedInUserId)
 {
     vector <Entry> entries;
+    int userId;
     Entry entry;
     CMarkup xml;
     xml.Load( FILE_NAME );
 
     while ( xml.FindElem("ENTRY" ))
     {
-    xml.IntoElem();
-    xml.FindElem( "ID" );
-    entry.setId(  atoi( MCD_2PCSZ(xml.GetData()) ) );
-    xml.FindElem( "USERID" );
-    if (FILE_NAME=="IncomeFile.xml")
-    {
-        entry.setType(0);
-    }
-    else if (FILE_NAME=="ExpenseFile.xml")
-    {
-        entry.setType(1);
-    }
-    entry.setUserId(  atoi( MCD_2PCSZ(xml.GetData()) ) );
-    xml.FindElem( "DESCRIPTION" );
-    entry.setDescription( xml.GetData() );
-    xml.FindElem( "AMOUNT" );
-    entry.setAmount( atoi( MCD_2PCSZ(xml.GetData()) ));
-    xml.FindElem( "DATE" );
-    entry.setDate( atoi( MCD_2PCSZ(xml.GetData()) ));
-    xml.OutOfElem();
-    entries.push_back(entry);
+        xml.IntoElem();
+        xml.FindElem( "USERID" );
+        userId = atoi( MCD_2PCSZ(xml.GetData()) );
+        if (userId == loggedInUserId)
+        {
+            entry.setUserId( userId );
+            xml.FindElem( "ID" );
+            entry.setId(  atoi( MCD_2PCSZ(xml.GetData()) ) );
+
+            if (FILE_NAME=="IncomeFile.xml")
+            {
+                entry.setType(0);
+            }
+            else if (FILE_NAME=="ExpenseFile.xml")
+            {
+                entry.setType(1);
+            }
+
+            xml.FindElem( "DESCRIPTION" );
+            entry.setDescription( xml.GetData() );
+            xml.FindElem( "AMOUNT" );
+            entry.setAmount( atoi( MCD_2PCSZ(xml.GetData()) ));
+            xml.FindElem( "DATE" );
+            entry.setDate( atoi( MCD_2PCSZ(xml.GetData()) ));
+            entries.push_back(entry);
+        }
+        xml.OutOfElem();
+
     }
 
     return entries;
